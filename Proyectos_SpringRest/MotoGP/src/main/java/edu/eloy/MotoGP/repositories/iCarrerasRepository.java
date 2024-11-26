43f0ef09a9aba9ace65d4996cd59ae7efecbaa32
soyel <soyeloygarciamartinez@gmail.com>
2024-11-26 13:08:19 +0100

    delete unused imports

--- Proyectos_SpringRest/MotoGP/src/main/java/edu/eloy/MotoGP/repositories/iCarrerasRepository.java
+++ Proyectos_SpringRest/MotoGP/src/main/java/edu/eloy/MotoGP/repositories/iCarrerasRepository.java
@@ -3,8 +3,6 @@ package edu.eloy.MotoGP.repositories;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import edu.eloy.MotoGP.entities.Carrera;
-import edu.eloy.MotoGP.entities.enumerated.Posicion;
-
 
 public interface iCarrerasRepository extends JpaRepository<Carrera, Integer> {
 
--- Proyectos_SpringRest/MotoGP/src/main/java/edu/eloy/MotoGP/repositories/iCircuitosRepository.java
+++ Proyectos_SpringRest/MotoGP/src/main/java/edu/eloy/MotoGP/repositories/iCircuitosRepository.java
@@ -1,11 +1,7 @@
 package edu.eloy.MotoGP.repositories;
 
-import org.hibernate.id.factory.spi.CustomIdGeneratorCreationContext;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import edu.eloy.MotoGP.entities.Circuito;
-import java.util.List;
-
 
 public interface iCircuitosRepository extends JpaRepository<Circuito, Integer>{
 
